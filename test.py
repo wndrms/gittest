03c988a973b90371656ba477113397679db8a3ff
wndrms <wndrms2006@naver.com>
2024-07-12 22:42:11 +0900

    test2

--- test.py
+++ test.py
@@ -16,3 +16,19 @@ def get_file_content_at_commit(repo_url, commit_hash, file_path):
         with open(file_path, 'r') as file:
             content = file.read()
         return content
+    
+def get_c_function_infos(node, source_code):
+        function_infos = {}
+
+        def walk_tree(node):
+            if node.type == 'function_definition':
+                name_node = node.child_by_field_name('declarator').child_by_field_name('declarator')
+                if name_node:
+                    function_name = source_code[name_node.start_byte:name_node.end_byte].decode('utf-8')
+                    function_infos[function_name] = (node.start_point[0], node.end_point[0])
+            
+            for child in node.children:
+                walk_tree(child)
+
+        walk_tree(node)
+        return function_infos
